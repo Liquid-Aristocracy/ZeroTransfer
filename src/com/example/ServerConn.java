a64c181f6073120f83a02caac95a75f37839c693
tankjb <wq1015381846@gmail.com>
2021-07-01 00:54:37 +0800

    Fix bugs

--- src/com/example/ServerConn.java
+++ src/com/example/ServerConn.java
@@ -1,13 +1,20 @@
 package com.example;
 
 import javax.swing.*;
+
+import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Vector;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.util.EntityUtils;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.example.ZeroManager;
@@ -15,18 +22,19 @@ import com.example.ZeroManager;
 public class ServerConn {
     private static String addr;
     private static int port;
+    private final String USER_AGENT = "Mozilla/5.0";
     public void init()
     {
-        this.addr = "server.zerotier" // Change this later
-        this.port = 8888
+        ServerConn.addr = "server.zerotier"; // Change this later
+        ServerConn.port = 8888;
         /* init */
     }
     //设置服务器地址和端口值，连接服务器声明在线，被 ZeroManager 的 init()调用。
 
     //先设一个返回值，不然会报错
-    public Vector<String> getOnlineNodeList()
+    public Vector<String> getOnlineNodeList() throws ClientProtocolException, IOException
     {
-        String url = this.addr + "/userList";
+        String url = ServerConn.addr + "/userList";
 
         HttpClient client = new DefaultHttpClient();
         HttpGet request = new HttpGet(url);
@@ -37,8 +45,9 @@ public class ServerConn {
         // Get json from response
         HttpResponse response = client.execute(request);
         // Need FastJson here
-        JSONObject json = new JSONObject(EntityUtils.toString(response.getEntity()));
-        Vector<String> list = new Vector<String>(JSON.parseArray(json.getJSONArray("users").toJSONString().String.class));
+        String data = EntityUtils.toString(response.getEntity());
+        JSONObject json = JSON.parseObject(data);
+        Vector<String> list = new Vector<String>(JSON.parseArray(json.getJSONArray("users").toJSONString(), String.class));
         
         return list;
     }
@@ -47,10 +56,11 @@ public class ServerConn {
 
     public void heartBeat()
     {
-        String url = this.addr + "/online?ip=" + ZeroManager.nodeIP;
-
-        HttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet(url);
+    	String url = ServerConn.addr + "/online";
+    	HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet();
+    	URI uri = new URIBuilder(request.getURI()).addParameter("ip", ZeroManager.nodeIP).addParameter("id", ZeroManager.nodeId).build();
+        ((HttpRequestBase) request).setURI(uri);
 
         request.addHeader("User-Agent", USER_AGENT);
 
@@ -61,4 +71,5 @@ public class ServerConn {
     /* 给服务器发送心跳包。*/
     /* 和 app 更新、校验码有关的方法添加在这里 */
 }
+
 
